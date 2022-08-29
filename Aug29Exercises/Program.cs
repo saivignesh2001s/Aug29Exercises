30e168b2e4a4e0d40c4fc55c0a94fdb1f1df1aa6
saivignesh2001s <saivignesh2001s@gmail.com>
2022-08-29 20:59:59 +0530

    cm

--- Aug29Exercises/Program.cs
+++ Aug29Exercises/Program.cs
@@ -6,69 +6,104 @@ namespace Aug29Exercises
     {
         static void Main(string[] args)
         {
-            Userstack<string> u = new Userstack<string>();
-            u.Push("America");
-            u.Push("Russia");
-            u.Push("India");
-            Console.WriteLine("Popped element");
-            u.Pop();
-            Console.WriteLine("Elements at present");
-            u.printData();
-            Console.WriteLine("Top element at present");
-            string k = u.Peek();
-            Console.WriteLine(k);
-            Console.ReadLine();
-           
+
+            // string linked List
+            GenericsLinkedList<string> stringLinkedList = new GenericsLinkedList<string>(); //object 1
+            string s1 = "String 1";
+            string s2 = "String 2";
+            string s3 = "String 3";
+            string s4 = "String 4";
+            stringLinkedList.AddHead(s1);
+            stringLinkedList.AddHead(s2);
+            stringLinkedList.AddHead(s3);
+            stringLinkedList.AddHead(s4);
+            //display List
+            foreach (string str in stringLinkedList)
+            {
+                Console.WriteLine(str);
+            }
+            //Integer LinkedList
+            GenericsLinkedList<int> integerList = new GenericsLinkedList<int>();
+            int n1 = 1;
+            int n2 = 2;
+            int n3 = 3;
+
+            integerList.AddHead(n1);
+            integerList.AddHead(n2);
+            integerList.AddHead(n3);
+
+            foreach (int Intger in integerList)
+            {
+                Console.WriteLine(Intger);
+            }
+
+
+            Console.ReadKey();
+
+
 
         }
 
     }
-    class Userstack<T>
+    class GenericsLinkedList<T>
     {
-        internal static int max = 1000;
-        T[] stack=new T[max];
-        int top=-1;
-        public void Push(T str)
+        class LinkedlistNode
         {
-            if (top >= 1000)
+            private LinkedlistNode next;
+            private T item;
+
+            public LinkedlistNode(T t)
             {
-                Console.WriteLine("can't push");
+                next = null;
+                item = t;
+
             }
-            else
+            public LinkedlistNode Next
             {
-                stack[++top] = str;
+                get
+                {
+                    return next;
+                }
+                set
+                {
+                    next = value;
+                }
+            }
+            public T Item
+            {
+                get
+                {
+                    return item;
+                }
+                set
+                {
+                    item = value;
+                }
             }
         }
-        public void Pop()
+        private LinkedlistNode head;
+        public GenericsLinkedList()
         {
-            if (top < 0)
-            {
-                Console.WriteLine("No elements there to pop");
-            }
-            else
-            {
-                T value=stack[top--];
-                Console.WriteLine(value);
-            }
+            head = null;
         }
-        public T Peek()
+        public void AddHead(T t)
         {
-            if (top < 0)
-            {
-                return default(T);
-            }
-            else
-            {
-                T value = stack[top];
-                return value;
-            }
+            LinkedlistNode node = new LinkedlistNode(t);
+            node.Next = head;
+            head = node;
         }
-        public void printData()
+        public IEnumerator<T> GetEnumerator()
         {
-            for(int i = top; i >= 0; i--)
+            LinkedlistNode current = head;
+            while (current != null)
             {
-                Console.WriteLine(stack[i]);
+                yield return current.Item;
+                current = current.Next;
             }
+
         }
+
     }
+
+
 }
